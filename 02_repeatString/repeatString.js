dfea060edb664e502df23fd1c9f81c64c6b97208
Wolter Daniel <wolterdaniel04@gmail,com>
2023-11-10 07:43:55 -0300

    Comprension de uso de tests driven developer (tdd)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(word, times) {
+let string = ""
+    if (times >= 0){
+        for(let i = 0; i < times; i++){
+            string += word;
+        }
+        return string;
+    }
+        return "ERROR"
+}
+;
 
 // Do not edit below this line
 module.exports = repeatString;
